080a553bb1d1eedce32ecca8829766b691f9b3d2
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-05-21 22:08:02 +0530

    ui Updated (citycompomponent)

--- src/Components/CityComponents.jsx
+++ src/Components/CityComponents.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
-import styled from 'styled-components'
-const WeatherLogo=styled.img`
-width:140px;
-height:140px;
-`
+import React from "react";
+import styled from "styled-components";
+const WeatherLogo = styled.img`
+  width: 140px;
+  height: 140px;
+  margin: 40px auto;
+`;
+const ChooseCityLabel = styled.span`
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+  margin:10px auto:
+`;
+const SearchBox = styled.form`
+  display:flex;
+  flex-direction:row;
+  border:black solid 1px;
+  border-radius:2px;
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+  margin:20px auto;
+  & input{
+    padding:10px;
+    font-size:14px;
+    border-none;
+    outline:none;
+    font-weight:Bold
+  }
+  & button{
+    padding:10px;
+    font-size:14px;
+    color:white;
+    background-color:black;
+    border-none;
+    outline:none;
+    font-weight:Bold;
+    cursor:pointer;
+  }
+`;
 
 
 const CityComponents = () => {
   return (
     <>
-    <WeatherLogo>
-      
-    </WeatherLogo>
-    </>
-  )
-}
+      <WeatherLogo src="\icons\perfect-day.svg"></WeatherLogo>
 
-export default CityComponents
+      <ChooseCityLabel>Find Weather Of Your City</ChooseCityLabel>
+      <SearchBox>
+        <input placeholder="City"/>
+        <button>Search</button>
+      </SearchBox>
+    </>
+  );
+};
+
+export default CityComponents;
--- src/Components/WeatherComponents.jsx
+++ src/Components/WeatherComponents.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import styled from "styled-components";
+
+const WeatherCondition=styled.div`
+display:flex;
+flex-direction:row;
+
+`
+const Condition=styled.span`
+display:flex;
+flex-direction:row;
+
+`
+
 const WeatherComponents = () => {
   return (
-    <div>WeatherComponent</div>
+   <>
+   <WeatherCondition>
+<Condition>30 C</Condition>
+   </WeatherCondition>
+   </>
   )
 }
 
