84efd1f680a8d21395f2a8ab18e10768bc8bc640
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-05-21 19:42:25 +0530

    Weather App

---
+++ src/Components/CityComponents.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+const WeatherLogo=styled.img`
+width:140px;
+height:140px;
+`
+
+
+const CityComponents = () => {
+  return (
+    <>
+    <WeatherLogo>
+      
+    </WeatherLogo>
+    </>
+  )
+}
+
+export default CityComponents
---
+++ src/Components/WeatherComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WeatherComponents = () => {
+  return (
+    <div>WeatherComponent</div>
+  )
+}
+
+export default WeatherComponents
